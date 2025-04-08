a97039820e2880487f3b96e443fe1c8748bbfcff
Kiersten Foulk <yoemail@iu.edu>
2025-04-08 16:58:41 -0400

    Updating file structure and testing integrations between pages for dynamically loading data

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,3 @@
 <?php
+?>
+<p>Just testing</p>
---
+++ public/pages/index.php
@@ -0,0 +1,29 @@
+<?php
+// Load board game data from local API
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>GameVault</title>
+        <link rel="stylesheet" href="../assets/css/style.css">
+        <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
+
+    </head>
+    <body>
+    <?php include 'includes/header.php'; ?>
+        <main>
+
+        </main>
+    <?php include 'includes/footer.php'; ?>
+    <script src="/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+
+</html>
+
+<?php
+
+
+?>
