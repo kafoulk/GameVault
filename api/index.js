dea15111759d9ba38ab6a618057539878d0de30f
Kiersten Foulk <yoemail@iu.edu>
2025-04-08 20:07:43 -0400

    Updating file structure and testing integrations between pages for dynamically loading data

--- api/index.js
+++ api/index.js
@@ -1,2 +1,16 @@
+const sqllite3 = require("sqllite3").verbose();
+
+// create a db
+
+const db = new sqllite3.Database("./games.sqllite", function (err) {
+    if (err) {
+        console.log("Error connecting to database");
+
+    } else
+        console.log("Database Connected: Games");
+});
+
+module.exports = db;
 
 
+const db = require("../database")
--- public/pages/404.php
+++ public/pages/404.php
@@ -2,3 +2,17 @@
 
 
 ?>
+
+<h1></h1>
+
+
+<body>
+<h1>404</h1>
+<p>Nice try, buddy. The page you are looking for cannot be found.</p>
+<p>Better luck next time!</p>
+<a class="home-link" href="/">Go Home</a>
+
+<script>
+    console.log("User landed on 404 page");
+</script>
+</body>
