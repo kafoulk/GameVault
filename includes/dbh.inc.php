5a91a95dfba060b42a9f38adf7d34a6bb2a32401
Kiersten Foulk <yoemail@iu.edu>
2025-04-17 12:32:17 -0400

    Added carousel to cart page and updated db connection

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,16 +1,15 @@
 <?php
 
-$host = "localhost";
-$port = 3306; // Default MySQL port
-$user = "root";
-$password = "";
-$dbname = "gamevault";
+$servername = "localhost";
+$port = 3306;
+$username = "root";
+$password = "root";
+$database = "gamevault";
 
-$dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database, $port);
 
-try {
-    $pdo = new PDO($dsn, $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Connection Failed" . $e->getMessage();
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
